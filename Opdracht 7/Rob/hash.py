d16ecd67bbb3b1b611ad4abebb3212515b4a33e9
RobvanCapelle <45008382+RobvanCapelle@users.noreply.github.com>
2018-12-16 12:36:24 +0100

    Iets verder met de hashmap

--- Opdracht 7/Rob/hash.py
+++ Opdracht 7/Rob/hash.py
@@ -1,7 +1,13 @@
-class Hash():
-    def __init__(self):
+class Hash(length):
+    def __init__()
+
+    def createHashmap(self, lenght):
 
 
 
 class Node():
     def __init__(self):
+
+
+test1 = Hash()
+test1.createHashmap(11)
