aa8922499ad325c15aea8f98464e39d6911329ed
trinm8 <tim.sanders@student.uantwerpen.be>
2018-12-20 13:27:13 +0100

    Create Leraar.py

---
+++ Opdracht 8/Arno/Leraar.py
@@ -0,0 +1,30 @@
+class Leraar:
+    def __init__(self, afkorting, naam, achternaam):
+        self.afkorting = afkorting
+        self.naam = naam
+        self.achternaam = achternaam
+
+    def getAfkorting(self):
+        return self.afkorting
+
+    def setAfkorting(self, afkorting):
+        self.afkorting = afkorting
+        return True
+
+    def getNaam(self):
+        return self.naam
+
+    def setNaam(self, naam):
+        self.naam = naam
+        return True
+
+    def getAchternaam(self):
+        return self.achternaam
+
+    def setAchternaam(self, achternaam):
+        self.achternaam = achternaam
+        return True
+
+
+def createLeraar(afkorting, naam, achternaam):
+    return Leraar(afkorting, naam, achternaam)
