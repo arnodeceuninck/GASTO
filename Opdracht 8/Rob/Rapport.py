6e396a7666a462be3812a46fef7116f6afdcdbad
trinm8 <tim.sanders@student.uantwerpen.be>
2018-12-20 13:15:15 +0100

    Create Rapport.py

---
+++ Opdracht 8/Rob/Rapport.py
@@ -0,0 +1,18 @@
+class Rapport:
+    def __init__(self, list, Zoeksleutel):
+        self.list = list
+        self.Zoeksleutel = Zoeksleutel
+
+    def getList(self):
+        return self.list
+
+    def setList(self, list):
+        self.list = list
+        return True
+
+    def getZoeksleutel(self):
+        return self.Zoeksleutel
+
+    def setZoeksleutel(self, Zoeksteutel):
+        self.Zoeksleutel = Zoeksteutel
+        return True
