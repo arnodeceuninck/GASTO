cf667c4205af9fd9a5a8656ee09573a29a8b3baf
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2018-12-22 21:52:19 +0100

    Filenames changed + Apart bestand class system (main = CLI)

--- Geheel/Main.py
+++ Geheel/Main.py
@@ -1,66 +1,6 @@
-import Leerling
-import Punt
-import Rapport
-import stack
-import Toets
-import Wachtlijst
-import Puntenlijst
+# Bestand voor Command Line Interface
 
-# Hier geen ADT Tabellen importeren! Dit gebeurt via TabelWrapper
-from TabelWrapper import *
-
-#Todo: retrieve info over de testen geven
-
-
-class system:
-    def __init__(self):
-        self.punten = TabelWrapper("ll") # dit is de create
-        self.toetsen = TabelWrapper("cl")
-        self.puntenlijst = TabelWrapper("bst")
-
-    def addPunt(self):
-        ID = input("ID: ")
-        self.punten.insert(Punt.createPunt(ID, input("Stamboomnummer: "),  # Value
-                                           input("Naam: "),
-                                           input("Waarde: "),
-                                           input("Timestamp: ")),
-                           ID)  # Key
-        return True
-
-    def deletePunt(self):
-        self.punten.delete(input("ID: "))
-        return True
-
-    def retrievePunt(self, key):
-        return self.punten.retrieve(key)
-
-    def removeAllPunten(self):
-        self.punten.destroy()
-
-    def addToets(self):
-        ID = input("Naam: ")
-        puntenlist = []
-        while True:
-            IDtest = input("ID test: ")
-            if IDtest == "":
-                break
-            else:
-                puntenlist.append(IDtest)
-        for i in range(len(puntenlist)-1):
-            if self.retrievePunt(puntenlist[i]) is not None:
-                puntenlist[i] = self.retrievePunt(puntenlist[i])
-            else:
-                print("ID van punt bestaat niet")
-
-        idascii = int(''.join(str(ord(c)) for c in ID))
-        #idascii = idascii[1:(len(idascii)-1)]
-
-        self.toetsen.insert(Toets.createToets(ID,
-                                              input("Maximum: "),
-                                              puntenlist), int(idascii))
-
-    def retrieveToets(self):
-        return self.toetsen.retrieve(input("Naam toets: "))
+from System import *
 
 
 geheel = system()
