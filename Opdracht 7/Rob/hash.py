ca0b79dd6a59b5fa6a3db8dfb88fe871add8e36e
RobvanCapelle <45008382+RobvanCapelle@users.noreply.github.com>
2018-12-22 20:02:55 +0100

    Hashmap\sepChain

--- Opdracht 7/Rob/hash.py
+++ Opdracht 7/Rob/hash.py
@@ -1,13 +1,133 @@
-class Hash(length):
-    def __init__()
+class TreeItem():
+    def __init__(self, item, key):
+        self.item = item
+        self.key = key
 
-    def createHashmap(self, lenght):
+class Hashmap():
+    def __init__(self, size, type):
+        self.tableSize = size
+        self.hashTable = [None] * (self.tableSize)
+        self.step = 1
+        self.type = type
+
+    def hashf(self, key):
+        return key % self.tableSize
+
+    def herHashf(self, key):
+        if self.type == 1:
+            return (key + self.step) % self.tableSize
+        elif self.type == 2:
+            return int((key + (self.step)**2) % self.tableSize)
+
+    def getPosition(self, key):
+        index = self.hashf(key)
+        if self.hashTable[index] == None:
+            return index
+        else:
+            if self.hashTable[index] != None:
+                while self.hashTable[index] != None:
+                    if self.hashTable[index] == None:
+                        return index
+                    else:
+                        if self.type == 1:
+                            index = self.herHashf(index)
+                        else:
+                            index = self.herHashf(key)
+                        if self.type == 2:
+                            self.step += 1
+            return index
+
+    def show(self):
+        i = 0
+        while i < self.tableSize:
+            if self.hashTable[i] != None:
+                print("Op plaats %s staat: %s" %(i, self.hashTable[i].item))
+            else:
+                print("Op plaats %s staat niets" %i)
+            i += 1
+
+    # def deleteHashmap(self):
+    #     for i in range(self.tableSize + 1):
+    #         delete(i)
+
+    def delete(self, key):
+        index = self.hashf(key)
+        if len(self.hashTable) == 0:
+            return False
+        if self.hashTable[index].key != key:
+            while self.hashTable[index].key != key:
+                index = self.herHashf(index)
+            self.hashTable[index] = None
+        else:
+            self.hashTable[index] = None
+
+    def retrieve(self, key):
+        index = self.hashf(key)
+        if len(self.hashTable) == 0:
+            return False
+        if self.hashTable[index].key != key:
+            while self.hashTable[index].key != key:
+                index = self.herHashf(index)
+            print(self.hashTable[index].item)
+        else:
+            print(self.hashTable[index].item)
+
+    def insert(self, key, item):
+        treeItem = TreeItem(item, key)
+        self.step = 1
+        self.hashTable[self.getPosition(key)] = treeItem
 
 
 
-class Node():
-    def __init__(self):
+size = input("Hoe groot moet de hashmap worden? ")
+select = input("Wilt u linair probing (1), qwadratic probing (2) of seperate chaining (3) gebruiken? ")
+h = Hashmap(int(size), int(select))
+#h.insert(searchKey, item)
+### (1) ###
+# h.insert(66, 0)
+# h.insert(63, 8)
+# h.insert(71, 5)
+# h.insert(10, 10)
+# h.insert(93, 6)
+# h.insert(28, 7)
+# h.insert(18, 9)
+# h.insert(72, 1)
+# h.insert(71, 2)
+# h.insert(5, 3)
+# h.insert(12, 4)
+# h.retrieve(12)
+# h.delete(12)
+# h.delete(18)
 
+### (1) delete met aanpassingen aan het invoeren ###
+# h.insert(66, 0)
+# h.insert(63, 8)
+# h.insert(71, 5)
+# h.insert(10, 10)
+# h.delete(71)
+# h.insert(93, 5)
+# h.insert(28, 6)
+# h.insert(18, 7)
+# h.insert(72, 9)
+# h.insert(71, 1)
+# h.insert(5, 2)
+# h.insert(12, 3)
 
-test1 = Hash()
-test1.createHashmap(11)
+### (2) ###
+# h.insert(66, 0)
+# h.insert(63, 8)
+# h.insert(71, 5)
+# h.insert(10, 10)
+# h.insert(93, 6)
+# h.insert(28, 7)
+# h.insert(18, 1)
+# h.insert(72, 4)
+# h.insert(71, 9)
+# h.insert(5, 3)
+# h.insert(12, 2)
+# h.retrieve(12)
+# h.delete(66)
+
+h.show()
+# h.deleteHashmap()
+# input()
